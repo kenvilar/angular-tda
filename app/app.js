350ffa03f215765c0b7487035bbf5c49752b091d
Ken Vilar <kenvilar@gmail.com>
2018-09-23 19:14:48 +0800

    Add the new data to the object

--- app/app.js
+++ app/app.js
@@ -12,9 +12,17 @@
 			};
 
 			self.addTodo = () => {
-				self.todoArr.push(self.newTodo);
+				self.todoArr.push({
+					title: self.newTodo.title,
+					priority: self.newTodo.priority,
+				});
 				console.log(self.todoArr);
-			}
+
+				self.newTodo = {
+					title: '',
+					priority: '',
+				};
+			};
 		},
 		templateUrl: '/app/templates/tda.html',
 	});
